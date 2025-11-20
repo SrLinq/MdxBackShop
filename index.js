1a50a5747d079aa7fdb8248c2c76e5ccf095cd03
srLin <bogfdan959@gmail.com>
2025-11-20 09:10:53 +0400

    Connection to db and importing all rrequired modules

--- index.js
+++ index.js
@@ -3,13 +3,33 @@ const http = require("http");
 const morgan = require("morgan");
 const fs = require("fs");
 var path = require("path");
-const  {ProductController}  = require("./product/productController");
-const productController = new ProductController();
+require("dotenv").config();
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb+srv://Cluster01780:Jshdh6367@project.spzmh.mongodb.net/";
 const app = express();
 app.use(express.json());
 app.use(morgan("short"));
 
-app.use("/product",productController.productRouter)
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
 
-http.createServer(app).listen(8080);
-  
+  next();  
+});
+let db;
+// Conect to MongoDB
+  MongoClient.connect(url, (error, client) => {
+    db = client.db("CST3144");
+    console.log("connected");
+  });
+
+
+
+
+app.listen(3000);
